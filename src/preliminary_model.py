ad139ab8c6717a02795ff14246f1c0d8a4843ffa
sum02dean <sum02dean@gmail.com>
2023-07-13 19:25:25 +0200

    added mlflow logging utils

--- src/preliminary_model.py
+++ src/preliminary_model.py
@@ -10,38 +10,43 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import optuna
 import argparse
+import mlflow
+import mlflow.sklearn
+from mlflow.tracking import MlflowClient
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+import logging
+from urllib.parse import urlparse
+from optuna.integration.mlflow import MLflowCallback
+
+
+
+# Establish mlflow logging config
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
 
 # Try optimising baseline model hyperparameters with Optuna - bayesian autoML methods
+def objective(trial):
 
-class Objective(object):
-    def __init__(self, study_name):
-
-        # Study name
-        self.study_name = study_name
-
+    with mlflow.start_run(run_name=str(trial.number)):
         # Features have already been pre-processed
-        self.train_x = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/scaled_unclipped_train_x.csv')
-        self.test_x = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/scaled_unclipped_test_x.csv')
+        train_x = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/scaled_unclipped_train_x.csv')
+        test_x = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/scaled_unclipped_test_x.csv')
 
         # Labels
-        self.train_y = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/train_y.csv')
-        self.test_y = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/test_y.csv')
+        train_y = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/train_y.csv')
+        test_y = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/test_y.csv')
 
         # Reformat x & y
-        self.train_labels = self.train_y.values.reshape(-1)
-        self.train_features = self.train_x.values
+        train_labels = train_y.values.reshape(-1)
+        train_features = train_x.values
 
-        self.test_labels = self.test_y.values.reshape(-1)
-        self.test_features = self.test_x.values
-
-    def __call__(self, trial):
-
-        # Get the train data
-        x, y = self.train_features, self.train_labels
+        test_labels = test_y.values.reshape(-1)
+        test_features = test_x.values
 
         # classifier_name = trial.suggest_categorical("classifier", ["SVC", "RandomForest"])
         classifier_name = 'RandomForest'
-        
+
         # Iterate over model specific hyper-parameters 
         if classifier_name == "SVC":
             svc_c = trial.suggest_float("svc_c", 1e-10, 1e10, log=True)
@@ -53,14 +58,20 @@ class Objective(object):
             classifier_obj = sklearn.ensemble.RandomForestClassifier(
                 max_depth=rf_max_depth, n_estimators=rf_n_estimators
             )
-        
+
         # Predict on the train set
-        trained_model = classifier_obj.fit(X=self.train_features, y=self.train_labels)
+        trained_model = classifier_obj.fit(X=train_features, y=train_labels)
 
         # Infer on the test set
-        test_pred = trained_model.predict(self.test_features)
-        test_acc = accuracy_score(self.test_labels, test_pred)
-        return test_acc
+        test_pred = trained_model.predict(test_features)
+        test_acc = accuracy_score(test_labels, test_pred)
+
+        # Log the hyperparameters and trial metrics with mlflow
+        signature = infer_signature(test_pred, test_pred)
+        mlflow.log_params(trial.params)
+        mlflow.log_metric('test_accuracy', test_acc)
+        mlflow.sklearn.log_model(classifier_obj, "random_forest", signature=signature)
+    return test_acc
 
 if __name__ == "__main__":
 
@@ -72,13 +83,15 @@ if __name__ == "__main__":
 
     # Setup dynamic logging
     study_name = args.study_name
-    objective = Objective(study_name=study_name)
     study = optuna.create_study(
         storage="sqlite:///db.sqlite3",
         study_name=study_name, direction='maximize')
     
-    #Optimize
-    study.optimize(objective, n_trials=100)
-
-# To run on terminal after training
-# optuna-dashboard sqlite:///db.sqlite3
+    # Optimize
+    mlflow.set_experiment(experiment_name='deans_run_2')
+    study.optimize(objective, n_trials=5)
+    
+    
+    # To run on terminal after training
+    # optuna-dashboard sqlite:///db.sqlite3
+    # mlflow ui
