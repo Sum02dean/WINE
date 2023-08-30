530072ab12d7cbf6f0638f6930e5b4d2724ee31d
sum02dean <sum02dean@gmail.com>
2023-08-30 16:27:32 +0200

    massive overhaul all params are not configured with json file, models are not inherited from abstract base models

--- src/preliminary_model.py
+++ src/preliminary_model.py
@@ -1,12 +1,10 @@
+""" Runs a Bayesian hyperparameter optimisation search using Optuna 
+to train and predict on a classicifation problem."""
+
 import pandas as pd
 import numpy as np
 import sklearn
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import optuna
 import argparse
@@ -19,9 +17,9 @@ import logging
 from urllib.parse import urlparse
 from optuna.integration.mlflow import MLflowCallback
 from mlflow.data.pandas_dataset import PandasDataset
-
-
-
+from random_forest import RandomForest
+from support_vector_classifier import SVClassifier
+import json
 
 # Establish mlflow logging config
 logging.basicConfig(level=logging.WARN)
@@ -31,73 +29,99 @@ logger = logging.getLogger(__name__)
 def objective(trial):
 
     with mlflow.start_run(run_name=str(trial.number)):
-        # Features have already been pre-processed
-        train_x = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/scaled_unclipped_train_x.csv')
-        test_x = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/scaled_unclipped_test_x.csv')
+
+        # Features
+        train_x_raw = pd.read_csv(data_params['X_train_path'])
+        test_x_raw  = pd.read_csv(data_params['X_test_path'])
 
         # Labels
-        train_y = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/train_y.csv')
-        test_y = pd.read_csv('/Users/sum02dean/projects/wine_challenge/WINE/data/test_y.csv')
-
-        # Reformat x & y
-        train_labels = train_y.values.reshape(-1)
-        train_features = train_x.values
-
-        test_labels = test_y.values.reshape(-1)
-        test_features = test_x.values
-
-        # classifier_name = trial.suggest_categorical("classifier", ["SVC", "RandomForest"])
-        classifier_name = 'RandomForest'
+        train_y_raw  = pd.read_csv(data_params['y_train_path'])
+        test_y_raw  = pd.read_csv(data_params['y_test_path'])
+        
+        # Sample these classifiers
+        classifier_name = trial.suggest_categorical("classifier", ["SVC", "RandomForest"])
 
         # Iterate over model specific hyper-parameters 
         if classifier_name == "SVC":
-            svc_c = trial.suggest_float("svc_c", 1e-10, 1e10, log=True)
-            svc_gamma = trial.suggest_float("svc_c", 1e-10, 1e10, log=True)
-            classifier_obj = sklearn.svm.SVC(C=svc_c, gamma=svc_gamma)
-        else:
-            rf_max_depth = trial.suggest_int("rf_max_depth", 2, 100, log=False)
-            rf_n_estimators = trial.suggest_int("rf_n_estimators", 2, 500, log=False)
-            classifier_obj = sklearn.ensemble.RandomForestClassifier(
-                max_depth=rf_max_depth, n_estimators=rf_n_estimators
-            )
+            print("Initializing model")
+            model = SVClassifier()
+            X_train, y_train = model.reshape_data(train_x_raw, train_y_raw)
+            X_test, y_test = model.reshape_data(test_x_raw , test_y_raw)
+
+            # Define optimizable hyperparameters ranges: C
+            lower_sample_c = model_params['svm']['c_lower_sample']
+            upper_sample_c = model_params['svm']['c_upper_sample']
+
+            # Define optimizable hyperparameters ranges: gamma
+            lower_sample_gamma = model_params['svm']['gamma_lower_sample']
+            upper_sample_gamma = model_params['svm']['gamma_upper_sample']
+
+            # Bayesian search over sampled space
+            svc_c = trial.suggest_float("svc_c", lower_sample_c, upper_sample_c, log=True)
+            svc_gamma = trial.suggest_float("svc_gamma", lower_sample_gamma, upper_sample_gamma, log=True)
+            classifier_obj = SVClassifier(C=svc_c, gamma=svc_gamma)
+
+        elif classifier_name == "RandomForest":
+            print("Initializing model")
+            model = RandomForest()
+            X_train, y_train = model.reshape_data(train_x_raw, train_y_raw)
+            X_test, y_test = model.reshape_data(test_x_raw , test_y_raw)
+
+            # Define optimizable hyperparameters ranges: max depth
+            lower_sample_max_depth = model_params['random_forest']['max_depth_lower_sample']
+            upper_sample_max_depth = model_params['random_forest']['max_depth_upper_sample']
+            
+            # Define optimizable hyperparameters ranges: n_estimators
+            lower_sample_n_estimators = model_params['random_forest']['n_estimators_lower_sample']
+            upper_sample_n_estimators = model_params['random_forest']['n_estimators_upper_sample']
+            
+            # Bayesian search over sampled space
+            rf_max_depth = trial.suggest_int("rf_max_depth", lower_sample_max_depth, upper_sample_max_depth, log=True)
+            rf_n_estimators = trial.suggest_int("rf_n_estimators", lower_sample_n_estimators, upper_sample_n_estimators, log=True)
+            classifier_obj = RandomForest(max_depth=rf_max_depth, n_estimators=rf_n_estimators)
 
         # Predict on the train set
-        trained_model = classifier_obj.fit(X=train_features, y=train_labels)
-
-        # Infer on the test set
-        test_pred = trained_model.predict(test_features)
-        test_acc = accuracy_score(test_labels, test_pred)
+        predictions = model.fit_predict(X_train, y_train, X_test)
+        test_acc = accuracy_score(y_test, predictions)
 
         # Log the datasets
         mlflow.log_artifacts('/Users/sum02dean/projects/wine_challenge/WINE/data')
 
         # Log the hyperparameters and trial metrics with mlflow
-        signature = infer_signature(test_pred, test_pred)
+        signature = infer_signature(predictions, predictions)
         mlflow.log_params(trial.params)
         mlflow.log_metric('test_accuracy', test_acc)
-        mlflow.sklearn.log_model(classifier_obj, "random_forest", signature=signature)
+        mlflow.sklearn.log_model(classifier_obj, classifier_name, signature=signature)
     return test_acc
-# 
+
+# Run Main
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--study_name", help="study name for optuna log",
-                        type=str)
-    
+    # Read in the configs file
+    parser.add_argument("--config_file", help="path to configs file",
+                        type=str, default="WINE/configs/config_file.json")
     args = parser.parse_args()
-    print(args)
+    
+    # Read in the parameters from configs file
+    configs = json.load(open("/Users/sum02dean/projects/wine_challenge/WINE/configs/config_file.json"))
+    mlflow_params = configs.get("mlflow_params")
 
-    # Setup dynamic logging
-    study_name = args.study_name
+    # Extract parameters
+    mlflow_params = configs.get("mlflow_params")
+    model_params = configs.get("model_params")
+    data_params = configs.get("data_params")
+    
+    # Create Optuna study
     study = optuna.create_study(
-        storage="sqlite:///db.sqlite3",
-        study_name=study_name, direction='maximize')
+        storage=mlflow_params["storage"],
+        study_name=mlflow_params["study_name"], direction='maximize')
     
     # Optimize
-    mlflow.set_experiment(experiment_name=args.study_name)
-    study.optimize(objective, n_trials=100)
+    mlflow.set_experiment(experiment_name=mlflow_params["study_name"])
+    study.optimize(objective, n_trials=mlflow_params["n_trials"])
     
     
-    # To run on terminal after training
-    # optuna-dashboard sqlite:///db.sqlite3
-    # mlflow ui
+# Run on terminal after running the prelimainary_model.py file:
+# optuna-dashboard sqlite:///db.sqlite3 (be inside the src directory)
+# mlflow ui (be inside the src directory)
