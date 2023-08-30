83f28a7d4d59f31397363151677290f2e8e8e445
sum02dean <sum02dean@gmail.com>
2023-08-30 16:56:21 +0200

    check linting

--- src/preliminary_model.py
+++ src/preliminary_model.py
@@ -1,5 +1,5 @@
 """ Runs a Bayesian hyperparameter optimisation search using Optuna 
-to train and predict on a classicifation problem - check for linting."""
+to train and predict on a classicifation problem - check for linting. """
 
 import pandas as pd
 import numpy as np
