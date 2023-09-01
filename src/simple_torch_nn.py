40582e75678e173dda8d6783e3ae723cf08c2ab2
sum02dean <sum02dean@gmail.com>
2023-09-01 12:26:55 +0200

    made lint suggestions

--- src/simple_torch_nn.py
+++ src/simple_torch_nn.py
@@ -10,7 +10,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.functional import one_hot
 
-
 # Define simle adaptable neural network architecture
 class SimpleNetwork(nn.Module):
     """A simple neural network"""
