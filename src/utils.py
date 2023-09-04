13e7a293cdb71fdc46e11abaf7703753b6df3503
sum02dean <sum02dean@gmail.com>
2023-09-04 18:39:52 +0200

    refactored for outputs

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 # Here is where we keeep all of the models
+import pandas as pd
 from abc import abstractmethod, ABC
 from sklearn.metrics import accuracy_score
 import numpy as np
@@ -15,28 +16,28 @@ class BaseModel(ABC):
         pass
 
     @abstractmethod
-    def fit(self, x, y) -> None:
+    def fit(self, x: np.array, y: np.array) -> None:
         pass
 
     @abstractmethod
-    def predict(self, x) -> np.array:
+    def predict(self, x: np.array) -> np.array:
         """
         Predicts the output for a given input.
         """
         pass
 
     @abstractmethod
-    def transform_data(self, x, y) -> None:
+    def transform_data(self, x: pd.DataFrame, y: pd.Series) -> None:
         """
         A method that reshapes or transforms the data.
         """
         pass
 
-    def fit_predict(self, x, y, x_val) -> None:
+    def fit_predict(self, x: np.array, y: np.array, x_val: np.array) -> np.ndarray:
         self.fit(x, y)
         return self.predict(x_val)
 
-    def report_accuracy(self, y_true, y_pred) -> float:
+    def report_accuracy(self, y_true: np.array, y_pred: np.array) -> float:
         test_acc = accuracy_score(y_true, y_pred)
         return test_acc
 
@@ -46,11 +47,11 @@ class MyDataset(torch.utils.data.Dataset):
         self.labels = labels
         self.features = features
 
-    def __len__(self):
+    def __len__(self) -> int:
         'Denotes the total number of samples'
         return np.shape(self.features)[0]
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> (np.array, np.array):
         'Generates one sample of data'
         x = self.features[index]
 
